a99fbe938bab229965bc58dc3af967bb19240b67
Guillermo Sosa <Guillermo Sosa>
2017-05-08 21:14:59 -0300

    Agregar archivos de proyecto.

---
+++ Estructuras_de_datos/TP4_01_Desviacion.vb
@@ -0,0 +1,22 @@
+﻿Module TP4_01_Desviacion
+    'Elaborar un algoritmo que permita cargar 5 números en un arreglo de 5 elementos; calcular la
+    'media y luego imprimir cada elemento del arreglo y la desviación que tiene respecto a la media.
+    'La MEDIA se obtiene la sumatoria de los todos elementos del arreglo y se divide por la cantidad
+    'de los mismos. La DESVIACION de cada elemento se obtiene restándole al elemento la
+    'MEDIA.
+    Sub Main()
+        Dim arreglo() As Integer = {0, 0, 0, 0}
+        Dim total, media As Integer
+        For i = 0 To 4
+            Console.Write("Ingrese un numero: ")
+            arreglo(i) = Console.ReadLine()
+            total += arreglo(i)
+        Next
+        media = total / 5
+        Console.WriteLine()
+        For i = 0 To 4
+            Console.WriteLine("El elemento {0} es {1} y su desviacion es {2}", i, arreglo(i), arreglo(i) - media)
+        Next
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ Estructuras_de_datos/TP4_02_Lista_de_Productos.vb
@@ -0,0 +1,26 @@
+﻿Module TP4_02_Lista_de_Productos
+    'Se tiene tres arrays cargados programáticamente con una lista de productos: el primero tiene el
+    'código, el segundo el nombre y el tercero el precio unitario. Repetitivamente se ingresa un
+    'código de producto y el programa muestra su descripción y precio, luego el usuario ingresa la
+    'cantidad. Esto sucede hasta que el código sea cero.
+    Sub Main()
+        Dim codigo() As Integer = {1, 2, 3}
+        Dim nombre() As String = {"Jabon", "Detergente", "Esponja"}
+        Dim precio() As Single = {23.5, 50.75, 10}
+        Dim cod, cant As Integer
+        Do
+            Console.Write("Ingrese codigo de producto: ")
+            cod = Console.ReadLine()
+            If cod > 0 Then
+                cod -= 1
+                Console.WriteLine("Descripcion: " & nombre(cod))
+                Console.WriteLine("Precio Unitario: $" & precio(cod).ToString)
+                Console.Write("Ingrese cantidad: ")
+                cant = Console.ReadLine()
+                Console.WriteLine("Subtotal: $" & precio(cod) * cant)
+                Console.WriteLine()
+                cod += 1
+            End If
+        Loop Until cod = 0
+    End Sub
+End Module
