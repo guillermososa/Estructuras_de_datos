5f5c0cd8e3e9f6953aeb28af6532a903f9525e4b
Guillermo Sosa <nbkguille@hotmail.com>
2017-06-05 17:44:33 -0300

    TP4_10_Nombres Completo

--- Estructuras_de_datos/Pruebas.vb
+++ Estructuras_de_datos/Pruebas.vb
@@ -1,37 +1,46 @@
 ﻿Module Pruebas
-    'Métodos Hash Table:  
-    '◦ Add(n,c) agrega un elemento.
-    '◦ Clear(): elimina todos los elementos.
-    '◦ Count: cantidad de elementos.
-    '◦ Item(c): devuelve un elemento por su índice o clave.
-    '◦ Remove(c): elimina un ítem.
-    '◦ Contains(c): permite establecer si un elemento existe.
-    '◦ DictionaryEntry: tipo de dato que permite manipular un elemento de la coleccion con sus atributos.
-    '◦ elemento.Key
-    '◦ elemento.Value
-
-    'Métodos Collection: 
-    '◦ Add(n,c) agrega un elemento.
+    'Métodos Collection : 
+    '◦ Add(n,c): agrega un elemento.
     '◦ Clear(): elimina todos los elementos.
     '◦ Count: cantidad de elementos.
     '◦ Item(c): devuelve un elemento por su índice o clave.
     '◦ Remove(c): elimina un ítem.
     '◦ Contains(c): permite establecer si un elemento existe.
 
-    'Métodos ArraList: 
-    '◦ Add(e) agrega un elemento.
+    'Métodos ArraList : 
+    '◦ Add(e): agrega un elemento.
     '◦ Count: cantidad de elementos.
     '◦ Remove(e): elimina un elemento.
     '◦ Item (i): retorna un elemento por un índice dado
-    '◦ (i): retorna un elemento por un índice dado
-    '◦ Insert (i,e): Inserta un elemento en el índice especificado.
-    '◦ Sort (): ordena los elementos.
-    '◦ RemoveAt (i): Quita el elemento situado en el índice especificado.
-    '◦ Reverse (): Invierte el orden de todos los elementos.
-    '◦ IndexOf(e): devuelve el índice de un elemento especificado.
+    '   ◦ (i): retorna un elemento por un índice dado
+    '   ◦ Insert (i,e): Inserta un elemento en el índice especificado.
+    '   ◦ Sort (): ordena los elementos.
+    '   ◦ RemoveAt (i): Quita el elemento situado en el índice especificado.
+    '   ◦ Reverse (): Invierte el orden de todos los elementos.
+    '   ◦ IndexOf(e): devuelve el índice de un elemento especificado.
+
+    'Métodos Hash Table:
+    '◦ Add(n,c): agrega un elemento.
+    '◦ Clear(): elimina todos los elementos.
+    '◦ Count: cantidad de elementos.
+    '◦ Item(c): devuelve un elemento por su índice o clave.
+    '◦ Remove(c): elimina un ítem.
+    '◦ Contains(c): permite establecer si un elemento existe.
+    '   ◦ DictionaryEntry: tipo de dato que permite manipular un elemento de la coleccion con sus atributos.
+    '   ◦ elemento.Key
+    '   ◦ elemento.Value
+
+    'Métodos Queue(FIFO) : 
+    '◦ Count: cantidad de elementos.
+    '   ◦ Enqueue(e): agrega un elemento a la coleccion.
+    '   ◦ Dequeue(): obtiene y elimina un elemento de la coleccion.
+
+    'Métodos Stack(LIFO) : 
+    '◦ Count: cantidad de elementos.
+    '   ◦ Push(e): agrega un elemento a la coleccion.
+    '   ◦ Pop(): obtiene y elimina un elemento de la coleccion.
 
     Sub Main()
 
     End Sub
-
 End Module
