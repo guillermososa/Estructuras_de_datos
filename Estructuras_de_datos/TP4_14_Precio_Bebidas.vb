b4428718608021b53cac7a0564b2a55d4f9f65f5
Guillermo Sosa <nbkguille@hotmail.com>
2017-06-12 19:50:59 -0300

    TP4_14_Precio_Bebidas Completo

--- Estructuras_de_datos/TP4_14_Precio_Bebidas.vb
+++ Estructuras_de_datos/TP4_14_Precio_Bebidas.vb
@@ -6,7 +6,143 @@
     'no exista y antes de editar o borrar verificar que exista.
     ' También disponer una opción que liste los artículos ordenados
     'por su nombre
-    Sub Main()
+    Private codigo, articulo As String
+    Private precio As Single
 
+    Enum Opciones As Byte
+        salir
+        agregar
+        editar
+        eliminar
+        eliminartodo
+        mostrar
+    End Enum
+
+    Sub Main()
+        Dim articulos As New Collection
+        Dim precios As New Collection
+        Dim opcion As Byte
+        Do
+            Console.Clear()
+            Console.WriteLine("0) Salir 1) Agregar 2)Editar 3)Eliminar 4)Eliminar todo 5)Mostrar")
+            Console.WriteLine("Cantidad de articulos cargados " & articulos.Count)
+            Console.Write("Ingrese Opcion: ")
+            opcion = Val(Console.ReadLine())
+            Select Case opcion
+                Case Opciones.salir
+                    opcion = 0
+                Case Opciones.agregar
+                    Agregar(articulos, precios)
+                Case Opciones.editar
+                    Editar(articulos, precios)
+                Case Opciones.eliminar
+                    Eliminar(articulos, precios)
+                Case Opciones.eliminartodo
+                    EliminarTodo(articulos, precios)
+                Case Opciones.mostrar
+                    Mostrar(articulos, precios)
+                Case Else
+                    Console.WriteLine("Ingrese nuevamente, valor incorrecto!.")
+            End Select
+        Loop Until opcion = 0
     End Sub
+
+    Private Sub Agregar(ByRef articulos As Collection, ByRef precios As Collection)
+        codigo = LeerTexto("Ingrese codigo: ")
+        If articulos.Contains(codigo) Then
+            Console.WriteLine("articulo ya existe!.")
+        Else
+            articulo = LeerTexto("Ingrese articulo: ")
+            precio = LeerTexto("Ingrese precio: ")
+            articulos.Add(articulo, codigo)
+            precios.Add(precio, codigo)
+            Console.WriteLine("Datos guardados correctamente(codigo: {0} articulo: {1} precio: {2}).", codigo, articulo, precio.ToString("###0.00"))
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Private Sub Editar(ByRef articulos As Collection, ByRef precios As Collection)
+        If Existen(articulos.Count) Then
+            codigo = LeerTexto("Ingrese codigo: ")
+            If Not articulos.Contains(codigo) Then
+                Console.WriteLine("articulo no encontrado!.")
+            Else
+                articulo = articulos.Item(codigo)
+                precio = precios.Item(codigo)
+                Console.WriteLine("articulo Actual: " & articulo)
+                Console.WriteLine("precio Actual: " & precio)
+                Console.Write("Ingrese Nuevo articulo: ")
+                articulo = Console.ReadLine()
+                Console.Write("Ingrese Nuevo precio: ")
+                precio = Console.ReadLine()
+                articulos.Remove(codigo)
+                precios.Remove(codigo)
+                articulos.Add(articulo, codigo)
+                precios.Add(precio, codigo)
+                Console.WriteLine("Datos editados correctamente(codigo: {0} articulo: {1} precio: {2}).", codigo, articulo, precio.ToString("###0.00"))
+            End If
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Private Sub Eliminar(ByRef articulos As Collection, ByRef precios As Collection)
+        If Existen(articulos.Count) Then
+            codigo = LeerTexto("Ingrese codigo: ")
+            If Not articulos.Contains(codigo) Then
+                Console.WriteLine("articulo no encontrado!.")
+                Console.ReadKey()
+            Else
+                articulo = articulos.Item(codigo)
+                precio = precios.Item(codigo)
+                articulos.Remove(codigo)
+                precios.Remove(codigo)
+                Console.WriteLine("Datos eliminados correctamente(codigo: {0} articulo: {1} precio: {2}).", codigo, articulo, precio.ToString("###0.00"))
+            End If
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Private Sub EliminarTodo(ByRef articulos As Collection, ByRef precios As Collection)
+        If Existen(articulos.Count) Then
+            articulos.Clear()
+            precios.Clear()
+            Console.WriteLine("Todos los datos fueron eliminados correctamente!.")
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Sub Mostrar(ByRef articulos As Collection, ByRef precios As Collection)
+        If Existen(articulos.Count) Then
+            Dim ArticulosOrdenados As New SortedList(Of String, Double)
+            For i As Integer = 1 To articulos.Count
+                ArticulosOrdenados.Add(articulos(i), precios(i))
+            Next
+            Console.WriteLine("Articulos Guardados: ")
+            For Each elemento As KeyValuePair(Of String, Double) In ArticulosOrdenados
+                Console.WriteLine("articulo: {0} precio: {1}).", elemento.Key, elemento.Value.ToString("###0.00"))
+            Next
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Function LeerTexto(mensaje As String) As String
+        Dim ingreso As String
+        Do
+            Console.Write(mensaje)
+            ingreso = Console.ReadLine()
+            If ingreso = "" Then
+                Console.Write("Ingrese nuevamente, valor nulo!.")
+            End If
+        Loop While ingreso = ""
+        Return ingreso
+    End Function
+
+    Private Function Existen(contenido As Boolean) As Boolean
+        If contenido = 0 Then
+            Console.WriteLine("No existen articulos!.")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
 End Module
--- Estructuras_de_datos/TP4_15_Turnos.vb
+++ Estructuras_de_datos/TP4_15_Turnos.vb
@@ -14,7 +14,60 @@
     'nombre del siguiente cliente a ser atendido. Si no hay compradores 
     'en espera informar “Sin clientes”.
     ' Utilizar Linkedlist.
-    Sub Main()
+    Enum Opciones As Byte
+        Salir
+        SacarTurno
+        LlamarProximo
+    End Enum
 
+    Sub Main()
+        Dim Clientes As New Queue
+        Dim Opcion As Byte
+        Do
+            Console.Clear()
+            Console.WriteLine("0)Salir 1)Sacar Turno 2)Llamar Proximo")
+            MostrarMensaje(Clientes.Count)
+            Opcion = Val(Leer("Ingrese Opcion: "))
+            Select Case Opcion
+                Case Opciones.Salir
+                    Opcion = 0
+                Case Opciones.SacarTurno
+                    SacarTurno(Clientes)
+                Case Opciones.LlamarProximo
+                    LlamarProximo(Clientes)
+                Case Else
+                    Console.WriteLine("Ingrese nuevamente, valor incorrecto!.")
+            End Select
+        Loop Until Opcion = 0
     End Sub
+
+    Private Sub MostrarMensaje(Cantidad As Byte)
+        Dim Mensaje As String
+        Mensaje = "Cantidad de Clientes en espera: " & Cantidad
+        If Cantidad = 0 Then
+            Mensaje &= " (Sin Clientes)"
+        End If
+        Console.WriteLine(Mensaje)
+    End Sub
+
+    Private Sub SacarTurno(ByRef Clientes As Queue)
+        Clientes.Enqueue(Leer("Ingrese su nombre: "))
+        Console.WriteLine("Aguarde un momento por favor, será llamado por uno de los empleados.")
+    End Sub
+
+    Private Sub LlamarProximo(ByRef Clientes As Queue)
+        If Not EsVacio(Clientes) Then
+            Console.WriteLine("Proximo Cliente: " & Clientes.Dequeue())
+            Console.ReadKey()
+        End If
+    End Sub
+
+    Private Function Leer(mensaje As String) As String
+        Console.Write(mensaje)
+        Return Console.ReadLine
+    End Function
+
+    Private Function EsVacio(Clientes As Queue) As Boolean
+        Return Clientes.Count = 0
+    End Function
 End Module
