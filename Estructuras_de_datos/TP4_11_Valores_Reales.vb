dde8c9b5618783a03dba6f1d3c66ac1fabcea872
Guillermo Sosa <nbkguille@hotmail.com>
2017-06-12 16:29:56 -0300

    TP4_11_Valores_Reales Completo

--- Estructuras_de_datos/TP4_11_Valores_Reales.vb
+++ Estructuras_de_datos/TP4_11_Valores_Reales.vb
@@ -4,6 +4,23 @@
     ' Si el valor ingresado es cero se quita un elemento.
     ' En todo momento informar la sumatoria total de los valores.
     Sub Main()
-
+        Dim Pila As New Stack(Of Double)
+        Dim valor As Double
+        Do
+            Console.Clear()
+            Console.WriteLine("Sumatoria de los valores: " & Pila.Sum)
+            Console.Write("Ingrese valor real: ")
+            valor = Console.ReadLine()
+            If valor = 0 And Not EsVacio(Pila) Then
+                Console.WriteLine("Se quitó el valor: " & Pila.Pop)
+            Else
+                Pila.Push(valor)
+                Console.WriteLine("Se ingresó el valor: " & valor)
+            End If
+        Loop Until EsVacio(Pila)
     End Sub
+
+    Private Function EsVacio(valor As Stack(Of Double)) As Boolean
+        Return valor.Count = 0
+    End Function
 End Module
